f8b59ad4b658decbb97bc6c3fe82797ddc3b8ece
Paul Edward Bennett <paul.e.bennett@icloud.com>
2018-11-10 20:27:47 +0000

    commit template changes to run.py & templates/base.html

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
-import os                  # Using operating system dependent functionality.
-from flask import Flask    # From module flask import class Flask.
+import os                                     # Using operating system dependent functionality.
+from flask import Flask, render_template      # From module flask (import class Flask, render_template).
 
-app = Flask(__name__)      # Construct an instance of Flask class for our webapp.
+app = Flask(__name__)                         # Construct an instance of Flask class for our webapp.
 
 # Route Decorator
-@app.route('/')            # URL '/' to be handled by main() route handler.
-                           # '/' navigates to localhost:5000.
-def hello():               # define function that returns "Hello World".
-    return "Hello Flask"
-if __name__ == '__main__': # __name__ will be equal to "__main__"
+@app.route('/')
+def index():
+    return render_template("index.html")      # Routing for index.html 
+    
+if __name__ == '__main__':                    # __name__ will be equal to "__main__"
 
 # If conditional statement is satisfied
     app.run(host=os.environ.get('IP'),        # launches the Flask's built-in development web server.  
                                               # host=os.environ.get('IP') gets IP Adress from operating system. 
             port=int(os.environ.get('PORT')), # os.environ.get('PORT') gets PORT we want to open.
-            debug=True)                       # # Enable reloader and debugger.p
+            debug=True)                       # Enable reloader and debugger.p
