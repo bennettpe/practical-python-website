498d83920f4c00d48112f7b837afdd9b7ba7051c
Paul Edward Bennett <paul.e.bennett@icloud.com>
2018-12-09 20:09:15 +0000

    commit changes to add geography2 category and fix issues with flask routing issue to geography2

--- run.py
+++ run.py
@@ -10,7 +10,6 @@ app.secret_key = "b'\xa0\xba+\xe5\xaa\x8b\xac\x01\x96\x1f<)86\x84\x04"
 # ------------------------ #
 # Writing to file process #
 # -------------------------#
-
 def write_to_file(filename, data):
     with open(filename, 'a') as file:
         file.writelines(data)
@@ -19,9 +18,8 @@ def write_to_file(filename, data):
 # ------------------------------- #
 # Count question_numbers process #
 # --------------------------------#
-
-def count_my_question_numbers():
-    with open('./data/geography1/geography1_questions.json', 'r') as json_data:
+def count_my_question_numbers(filename):
+    with open(filename, 'r') as json_data:
         data = json.load(json_data)
     return len(data)
 
@@ -29,51 +27,45 @@ def count_my_question_numbers():
 # -------------------- #
 # Create date process #
 # ---------------------#
-
 def create_date():
     date_format = '%d/%m/%y'
     get_date = datetime.now().strftime(date_format)
     return get_date
 
-
+# ---------------------------#
+# Get last incorrect answer #
+# ---------------------------#
+def get_geography_incorrect_answer(filename):
+    answers = []
+    with open(filename, 'r') as incorrect_answer:
+        answers = [row for row in incorrect_answer]
+        return answers[-1:]
+        
 # --------------------------------#
 # Route Decorator for index.html #
 # --------------------------------#
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-# ---------------------------#
-# Get last incorrect answer #
-# ---------------------------#
-
-def get_geography1_incorrect_answer():
-    answers = []
-    with open('./data/geography1/geography1_incorrect_answer.txt', 'r') as incorrect_answer:
-        answers = [row for row in incorrect_answer]
-        return answers[-1:]
-
-
 # --------------------------------------#
 # Get Username for geography1 category #
 # --------------------------------------#
-
 @app.route('/geography1_get_username', methods=['GET', 'POST'])
 def geography1_get_username():
 
-    catname = 'Geography 1'
+    catname  = 'Geography 1'
     filename = './data/geography1/geography1_username.txt'
-    imgname = './static/img/portfolio/thumbnails/image-from-rawpixel-id-90517.jpg'
+    imgname  = './static/img/portfolio/thumbnails/image-from-rawpixel-id-90517.jpg'
 
     if request.method == 'POST':
-        with open(filename, 'a') as username_list:username_list.write(request.form['geography1_username']+ '\n')  
-        return redirect(request.form['geography1_username'])
-
+        with open(filename, 'a') as username_list:
+             username_list.write(request.form['geography1_username']+ '\n')  
         open('./data/geography1/geography1_correct_answer.txt', 'w').close()  
         open('./data/geography1/geography1_incorrect_answer.txt', 'w').close()
         open('./data/geography1/geography1_final_score.txt', 'w').close()
+        return redirect(url_for('geography1_user', geography1_username = request.form['geography1_username']))
     return render_template('geography1_get_username.html',category=catname, img_id=imgname)
 
 
@@ -81,8 +73,7 @@ def geography1_get_username():
 # Read   geography1 jsonfile          #
 # Render geography1_quiz.html page    #
 # -------------------------------------#
-
-@app.route('/<geography1_username>', methods=['GET', 'POST'])
+@app.route('/geography1_user<geography1_username>', methods=['GET', 'POST'])
 def geography1_user(geography1_username):
 
     catname = 'Geography 1'
@@ -97,8 +88,8 @@ def geography1_user(geography1_username):
         index = 0
         score = 0
         correct_answer = questions[index]['answer']
-        count_questions = count_my_question_numbers()
-        incorrect_answer = get_geography1_incorrect_answer()
+        count_questions = count_my_question_numbers('./data/geography1/geography1_questions.json')
+        incorrect_answer = get_geography_incorrect_answer('./data/geography1/geography1_incorrect_answer.txt')
         todays_date = create_date()
 
         open('./data/geography1/geography1_correct_answer.txt', 'a')  
@@ -112,14 +103,12 @@ def geography1_user(geography1_username):
             username_answer = request.form['username_answer'].title()
 
             if username_answer == correct_answer:
-                write_to_file('./data/geography1/geography1_correct_answer.txt'
-                              , username_answer + '\n') 
+                write_to_file('./data/geography1/geography1_correct_answer.txt', username_answer + '\n') 
                 index += 1  
                 score += 1
                 check_correct = True
             else:
-                write_to_file('./data/geography1/geography1_incorrect_answer.txt'
-                              , username_answer + '\n') 
+                write_to_file('./data/geography1/geography1_incorrect_answer.txt', username_answer + '\n') 
                 index += 1
                 score = score
                 check_correct = False
@@ -133,11 +122,10 @@ def geography1_user(geography1_username):
                                   + geography1_username + ':'
                                   + str(count_questions) + ':'
                                   + request.form['score'] + '\n')  
-                    write_to_file('./data/geography1/geography1_final_score.txt'
-                                  , request.form['score'] + '\n')  
+                    write_to_file('./data/geography1/geography1_final_score.txt', request.form['score'] + '\n')  
                     return redirect('geography1_completed_quiz')
 
-    incorrect_answer = get_geography1_incorrect_answer()
+    incorrect_answer = get_geography_incorrect_answer('./data/geography1/geography1_incorrect_answer.txt')
 
     return render_template('geography1_quiz.html',
                             category = catname,
@@ -152,19 +140,100 @@ def geography1_user(geography1_username):
                             username = geography1_username,
                            )
 
+                           
+# --------------------------------------#
+# Get Username for geography2 category #
+# --------------------------------------#
+@app.route('/geography2_get_username', methods=['GET', 'POST'])
+def geography2_get_username():
 
-# Route Decorator for geography2 username
+    catname = 'Geography 2'
+    filename = './data/geography2/geography2_username.txt'
+    imgname = './static/img/portfolio/thumbnails/image-from-rawpixel-id-433915.jpg'
 
-@app.route('/geography2_username')
-def geography2_username():
-    image = \
-        'static/img/portfolio/thumbnails/image-from-rawpixel-id-433915.jpg'  # Image Filename
-    return render_template('geography2_username.html',
-                           category='Geography 2', img_id=image)  # Routing for geography2_username.html
+    if request.method == 'POST':
+        with open(filename, 'a') as username_list:
+            username_list.write(request.form['geography2_username']+ '\n')  
+        open('./data/geography2/geography2_correct_answer.txt', 'w').close()  
+        open('./data/geography2/geography2_incorrect_answer.txt', 'w').close()
+        open('./data/geography2/geography2_final_score.txt', 'w').close()
+        return redirect(url_for('geography2_user', geography2_username = request.form['geography2_username']))
+    return render_template('geography2_get_username.html',category=catname, img_id=imgname)
 
 
+# -------------------------------------#
+# Read   geography2 jsonfile          #
+# Render geography2_quiz.html page    #
+# -------------------------------------#
+@app.route('/geography2_user<geography2_username>', methods=['GET', 'POST'])
+def geography2_user(geography2_username):
+
+    catname = 'Geography 2'
+    filename = './data/geography2/geography2_questions.json'
+    imgname = './static/img/portfolio/thumbnails/image-from-rawpixel-id-433915.jpg'
+    check_correct = []
+    questions = []
+
+    with open(filename, 'r') as questions_file:  
+        questions = json.load(questions_file)
+
+        index = 0
+        score = 0
+        correct_answer = questions[index]['answer']
+        count_questions = count_my_question_numbers('./data/geography2/geography2_questions.json')
+        incorrect_answer = get_geography_incorrect_answer('./data/geography2/geography2_incorrect_answer.txt')
+        todays_date = create_date()
+
+        open('./data/geography2/geography2_correct_answer.txt', 'a')  
+        open('./data/geography2/geography2_incorrect_answer.txt', 'a')
+        open('./data/geography2/geography2_final_score.txt', 'a')
+
+        if request.method == 'POST':
+            index = int(request.form['index'])
+            score = int(request.form['score'])
+            correct_answer = request.form['correct_answer']
+            username_answer = request.form['username_answer'].title()
+
+            if username_answer == correct_answer:
+                write_to_file('./data/geography2/geography2_correct_answer.txt', username_answer + '\n') 
+                index += 1  
+                score += 1
+                check_correct = True
+            else:
+                write_to_file('./data/geography2/geography2_incorrect_answer.txt', username_answer + '\n') 
+                index += 1
+                score = score
+                check_correct = False
+
+            # When all questions have been answered, save final score in geography2_leaders.txt #
+
+            if request.method == 'POST':
+                if index == 18:  # Total number of questions for category
+                    write_to_file('./data/geography2/geography2_leaders.txt'
+                                  , str(todays_date) + ':'
+                                  + geography2_username + ':'
+                                  + str(count_questions) + ':'
+                                  + request.form['score'] + '\n')  
+                    write_to_file('./data/geography2/geography2_final_score.txt', request.form['score'] + '\n')  
+                    return redirect('geography2_completed_quiz')
+
+    incorrect_answer = get_geography_incorrect_answer('./data/geography2/geography2_incorrect_answer.txt')
+
+    return render_template('geography2_quiz.html',
+                            category = catname,
+                            img_id = imgname,
+                            geography2_questions = questions,
+                            index = index,
+                            score = score,
+                            check_correct = check_correct,
+                            correct_answer = questions[index]['answer'],
+                            count_questions = count_questions,
+                            incorrect_answer = incorrect_answer,
+                            username = geography2_username,
+                           )
+
+                           
 # Route Decorator for populous username
-
 @app.route('/populous_username')
 def populous_username():
     image = \
@@ -175,7 +244,6 @@ def populous_username():
 
 
 # Route Decorator for capitals username
-
 @app.route('/capitals_username')
 def capitals_username():
     image = \
@@ -186,7 +254,6 @@ def capitals_username():
 
 
 # Route Decorator for islands username
-
 @app.route('/islands_username')
 def islands_username():
     image = \
@@ -197,7 +264,6 @@ def islands_username():
 
 
 # Route Decorator for highest questions
-
 @app.route('/highest_username')
 def highest_username():
     image = \
@@ -206,9 +272,5 @@ def highest_username():
                            category='Which City Is Highest',
                            img_id=image)  # Routing for highest_username.html
 
-if __name__ == '__main__':  # __name__ will be equal to "__main__"
-
-# If conditional statement is satisfied
-
-    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT'
-            )), debug=True)  
+if __name__ == '__main__': 
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)  
